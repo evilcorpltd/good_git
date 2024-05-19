8bc47b0a7cd954dc952d4e73023ed57b7385a628
Johan Norberg <johnor@users.noreply.github.com>
2024-05-19 12:45:09 +0200

    Add parent lookup of the .git directory

--- src/repo.rs
+++ src/repo.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 static GIT_FOLDER_NAME: &str = ".git";
 
 #[derive(Debug)]
@@ -13,10 +15,35 @@ impl Repo {
     }
 
     pub fn from_dir(path: &std::path::Path) -> Option<Self> {
-        Some(Repo::new(path))
+        let path = fs::canonicalize(path).ok()?;
+        let git_dir = path.ancestors().find(|&d| {
+            let git_dir = d.join(GIT_FOLDER_NAME);
+            git_dir.exists()
+        });
+        git_dir.map(Repo::new)
     }
 
     pub fn git_dir(&self) -> std::path::PathBuf {
         self.root.join(GIT_FOLDER_NAME)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_dir_in_sub_dir() {
+        let tmpdir = tempfile::tempdir().unwrap().path().to_path_buf();
+        let git_dir = tmpdir.join(GIT_FOLDER_NAME);
+        std::fs::create_dir_all(&git_dir).unwrap();
+
+        let cwd_dir = tmpdir.join("hello/from/nested/dir");
+        std::fs::create_dir_all(&cwd_dir).unwrap();
+
+        let repo = Repo::from_dir(&cwd_dir).unwrap();
+
+        assert_eq!(tmpdir, repo.root);
+        assert_eq!(git_dir, repo.git_dir());
     }
 }
