3db645d095d1fa83654e06c828d7d393eb2ae929
Johan Norberg <johnor@users.noreply.github.com>
2024-06-09 19:32:44 +0200

    Add integration tests and move tests from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -66,9 +66,6 @@ pub fn cat_file(repo: &Repo, object_hash: &str, stdout: &mut dyn io::Write) -> R
 
 #[cfg(test)]
 mod tests {
-    use flate2::{write::ZlibEncoder, Compression};
-    use std::io::prelude::*;
-
     use super::*;
 
     #[test]
@@ -89,72 +86,5 @@ mod tests {
         // From https://git-scm.com/book/sv/v2/Git-Internals-Git-Objects
         hash_object(&mut "test content\n".as_bytes(), &mut stdout).unwrap();
         assert_eq!(stdout, b"d670460b4b4aece5915caf5c68d12f560a9fe3e4\n");
-    }
-
-    #[test]
-    fn test_cat_file() {
-        let tmpdir = tempfile::tempdir().unwrap();
-        let path = tmpdir
-            .path()
-            .to_path_buf()
-            .join(".git/objects/d6/70460b4b4aece5915caf5c68d12f560a9fe3e4");
-        let mut stdout = Vec::new();
-
-        let prefix = path.parent().unwrap();
-        std::fs::create_dir_all(prefix).unwrap();
-
-        // Compress the content of the blob object and write to file
-        let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-        e.write_all(b"blob 13\0test content\n").unwrap();
-        let compressed = e.finish().unwrap();
-        std::fs::write(&path, compressed).unwrap();
-
-        cat_file(
-            &Repo::new(tmpdir.path()),
-            "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
-            &mut stdout,
-        )
-        .unwrap();
-        assert_eq!(stdout, b"test content\n\n");
-    }
-
-    #[test]
-    fn test_cat_file_short_hash() {
-        let tmpdir = tempfile::tempdir().unwrap();
-        let path = tmpdir
-            .path()
-            .to_path_buf()
-            .join(".git/objects/d6/70460b4b4aece5915caf5c68d12f560a9fe3e4");
-        let mut stdout = Vec::new();
-
-        let prefix = path.parent().unwrap();
-        std::fs::create_dir_all(prefix).unwrap();
-
-        // Compress the content of the blob object and write to file
-        let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-        e.write_all(b"blob 13\0test content\n").unwrap();
-        let compressed = e.finish().unwrap();
-        std::fs::write(&path, compressed).unwrap();
-
-        cat_file(&Repo::new(tmpdir.path()), "d670", &mut stdout).unwrap();
-        assert_eq!(stdout, b"test content\n\n");
-    }
-
-    #[test]
-    fn test_cat_file_fails_if_rev_not_found() {
-        let tmpdir = tempfile::tempdir().unwrap();
-        let path = tmpdir
-            .path()
-            .to_path_buf()
-            .join(".git/objects/d6/70460b4b4aece5915caf5c68d12f560a9fe3e4");
-        let mut stdout = Vec::new();
-
-        let prefix = path.parent().unwrap();
-        std::fs::create_dir_all(prefix).unwrap();
-
-        let err = cat_file(&Repo::new(tmpdir.path()), "d6", &mut stdout)
-            .unwrap_err()
-            .to_string();
-        assert_eq!(err, "Object not found");
     }
 }
